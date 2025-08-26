a50547e5210cc4a6be1a4c346035fcc20cba6fa2
Jeison Altamirano <jeison@JeisPC.>
2025-08-25 22:52:13 -0600

    Fix iron-session typing (SessionOptions)

--- lib/session.ts
+++ lib/session.ts
@@ -1,11 +1,21 @@
-import { IronSessionOptions } from 'iron-session'
-
-export const sessionOptions: IronSessionOptions = {
-  password: process.env.SESSION_SECRET || 'complex_password_at_least_32_chars_long',
-  cookieName: 'serbiz_session',
-  cookieOptions: { secure: process.env.NODE_ENV === 'production' },
-}
+// lib/session.ts
+import type { SessionOptions } from 'iron-session'
 
 export type SessionData = {
-  user?: { id: string; username: string; role: 'user' | 'vendor' | 'admin'; locale: 'es'|'en' }
+  user?: {
+    id: string
+    username: string
+    role: 'user' | 'vendor' | 'admin'
+    locale: 'es' | 'en'
+  }
+}
+
+export const sessionOptions: SessionOptions = {
+  password:
+    process.env.SESSION_SECRET ||
+    'complex_password_at_least_32_chars_long',
+  cookieName: 'serbiz_session',
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production',
+  },
 }
